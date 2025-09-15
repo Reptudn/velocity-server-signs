cb5e7912787fd827b58de78c1b578817ba096a9b
Reptudn <jonas.kauker2003@gmail.com>
2025-09-15 11:09:57 +0200

    initial project

---
+++ src/main/java/de/reptudn/velocityServerSigns/VelocityServerSigns.java
@@ -0,0 +1,17 @@
+package de.reptudn.velocityServerSigns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VelocityServerSigns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
